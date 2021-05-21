86a8c96192af51782c5b1109af4eea8a4c9c16d6
wesley <wesleysilvadesousa@gmail.com>
2021-05-21 16:20:45 -0300

    feat: send reset password email

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,6 +1,7 @@
 'use strict'
 const crypto = require('crypto')
 const User = use('App/Models/User')
+const Mail = use('Mail')
 class ForgotPasswordController {
     async store({ request, response }) {
         try {
@@ -8,7 +9,17 @@ class ForgotPasswordController {
             const user = await User.findByOrFail('email', email)
             user.token = crypto.randomBytes(10).toString('hex')
             user.token_created_at = new Date()
-            await user.save() 
+            await user.save()
+            await Mail.send(
+                ['emails.forgot_password'],
+                { email, token: user.token, link: `${request.input('redirect_url')}?token=${user.token}`},
+                message => {
+                    message
+                        .to(user.email)
+                        .from('tgl@email.com', 'TGL')
+                        .subject('Recuperação de Senha')
+                }
+            ) 
             return user
         } catch (err) {
             return response
