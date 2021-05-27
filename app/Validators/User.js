0f420dc7d957153107e50c8c9c8ad86b48100d03
wesley <wesleysilvadesousa@gmail.com>
2021-05-27 11:41:44 -0300

    refactor: validator messages

--- app/Validators/User.js
+++ app/Validators/User.js
@@ -12,6 +12,18 @@ class User {
       password: 'required'
     }
   }
+
+  get messages () {
+    return {
+      'username.required': 'You must provide a username.',
+      'username.username': 'You must provide a valid username.',
+      'username.unique': 'This usename is already registered.',
+      'email.required': 'You must provide a email address.',
+      'email.email': 'You must provide a valid email address.',
+      'email.unique': 'This email is already registered.',
+      'password.required': 'You must provide a password'
+    }
+  }
 }
 
 module.exports = User
